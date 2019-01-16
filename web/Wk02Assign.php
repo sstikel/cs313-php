967a84bedf6cbfe18b0876230e97470eee830d02
Samuel Gay <sstikel@gmail.com>
2019-01-16 15:20:00 -0700

    Initial files for main homepage

---
+++ web/Wk02Assign.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home-SamG-CS313</title>
+    
+    <!--TODO External Stylesheet-->
+    <!--TODO Include colors, images, personality, creativity (keep professional)-->
+    <link rel="stylesheet" type="text/css" href="Wk02SS.css"><!--TODO Alter so it works on Heroku-->
+    <!--TODO Add something dynamic (mouse-overs, on-click, on-change, etc)-->
+    <script src="Wk02JS.js"></script><!--TODO Alter so it works on Heroku-->
+  </head>
+
+  <header>
+    <h1>CS313 - Sam Gay</h1>
+    
+    <!--I would like a menu along the top-->
+    <div class="topMenu">
+      <!--<?php include 'Wk02TopMenu.php'; ?>-->
+    </div>
+  </header>
+
+  <body>
+    <div id="dAssign">
+      <h3>Assignments</h3>
+      <!--TODO Links to assignments-->
+      <!--For now this page can just say "Coming Soon"-->
+      <ul>
+        <li><a href="https://warm-refuge-37557.herokuapp.com/hello.html">HelloWorld!</a></li>
+      </ul>
+    </div>
+  </body>
+
+  <footer>
+  
+  </footer>
+  
+</html>
+
+
+<!--TODO Change file to PHP-->
+<!--TODO Add PHP content-->
+
+<!--TODO Make sure JS and CSS pages make it to server-->
+
+
+<!---->
---
+++ web/Wk02Home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home-SamG-CS313</title>
+    
+    <!--TODO External Stylesheet-->
+    <!--TODO Include colors, images, personality, creativity (keep professional)-->
+    <link rel="stylesheet" type="text/css" href="Wk02SS.css"><!--TODO Alter so it works on Heroku-->
+    <!--TODO Add something dynamic (mouse-overs, on-click, on-change, etc)-->
+    <script src="Wk02JS.js"></script><!--TODO Alter so it works on Heroku-->
+  </head>
+
+  <header>
+    <!--I would like a menu along the top-->
+    <div class="topMenu">
+      <!--<?php include 'Wk02TopMenu.php'; ?>-->
+    </div>
+  </header>
+
+  <body>
+    <!--TODO Add image (or more)-->
+    <img src="" alt="">
+  </body>
+
+  <footer>
+  
+  </footer>
+  
+</html>
+
+
+<!--TODO Change file to PHP-->
+<!--TODO Add PHP content-->
+
+<!--TODO Make sure JS and CSS pages make it to server-->
+
+
+<!---->
