61a16440c5160bf5cd6ae850e9a24bcb34e80c17
Samuel Gay <sstikel@gmail.com>
2019-01-17 14:54:41 -0700

    adding php include and testing top menu

--- web/Wk02Home.php
+++ web/Wk02Home.php
@@ -15,7 +15,7 @@
   <header>
     <!--I would like a menu along the top-->
     <div class="topMenu">
-    <!-- include 'Wk02TopMenu.php'; --><!--Add php tags-->
+    <?php include 'Wk02TopMenu.php'; ?>
     </div>
   </header>
   <hr>
--- web/Wk02TopMenu.php
+++ web/Wk02TopMenu.php
@@ -1,3 +1,5 @@
 <?php
-  echo "<a href="Wk02Home.php">Home</a> <a href="Wk02Assign.php">Assignments</a>";
+  echo "/*<a href="Wk02Home.php">Home</a> <a href="Wk02Assign.php">Assignments</a>";*/
+  
+  echo "This should be the top menu";
 ?>
