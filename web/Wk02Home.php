e6086b2fde2fc38f476f4c7a2c2cf4768c885503
Samuel Gay <sstikel@gmail.com>
2019-01-17 14:47:24 -0700

    remove php script to test

--- web/Wk02Home.php
+++ web/Wk02Home.php
@@ -15,7 +15,7 @@
   <header>
     <!--I would like a menu along the top-->
     <div class="topMenu">
-      <?php include 'Wk02TopMenu.php'; ?>
+    <!--  <?php include 'Wk02TopMenu.php'; ?>-->
     </div>
   </header>
   <hr>
