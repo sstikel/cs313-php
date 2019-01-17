6820f5a479a1043d8bfa01aeb132b17dbc96b178
Samuel Gay <sstikel@gmail.com>
2019-01-17 14:38:59 -0700

    Photos and home.php update

--- web/Wk02Home.php
+++ web/Wk02Home.php
@@ -6,7 +6,9 @@
     <!--TODO External Stylesheet-->
     <!--TODO Include colors, images, personality, creativity (keep professional)-->
     <link rel="stylesheet" type="text/css" href="Wk02SS.css"><!--TODO Alter so it works on Heroku-->
+    
     <!--TODO Add something dynamic (mouse-overs, on-click, on-change, etc)-->
+    
     <script src="Wk02JS.js"></script><!--TODO Alter so it works on Heroku-->
   </head>
 
@@ -16,14 +18,26 @@
       <?php include 'Wk02TopMenu.php'; ?>
     </div>
   </header>
+  <hr>
 
   <body>
     <!--TODO Add image (or more)-->
-    <img src="" alt="">
+    <img id="imgProfile" src="BlueShirt.png" alt="Sam Gay">
+    <p id="bio">
+      <strong>Name: Sam Gay<strong><br>
+      Hometown: Sahuarita, AZ<br>
+      Major: Software Engineering (2020)<br>
+      Interests:Sam loves most anything in the outdoors; whether it be hiking, backpacking, shooting, camping, water skiing, etc.<br>
+      Goals: Find a software job, get paid, have kids, take kids hiking.<br>
+    </p>
+    <div id="dImages">
+      <img src="SamLiz.jpg" alt="Sam and Liz"><img src="Hiking.jpg" alt="Hiking"><img src="Ballet.jpg" alt="Ballet">
+    </div>
   </body>
 
+  <hr>
   <footer>
-  
+  <a href="https://www.linkedin.com/in/samuel-gay-69522490/">LinkedIn</a>, <a href="https://github.com/sstikel">GitHub</a>
   </footer>
   
 </html>
