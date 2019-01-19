aa94030f68d0f8638fd7628c16766c350a5bc128
Samuel Gay <sstikel@gmail.com>
2019-01-18 19:16:49 -0700

    updated js

--- web/Wk02JS.js
+++ web/Wk02JS.js
@@ -1,7 +1,7 @@
 function benignFunction() {
-  location.replace("https://warm-refuge-37557.herokuapp.com/noTouch.html") 
+  window.location.href="https://warm-refuge-37557.herokuapp.com/noTouch.html"; 
 }
 
 function undoIt() {
- location.replace("https://warm-refuge-37557.herokuapp.com/Wk02Home.php") 
+ window.location.href="https://warm-refuge-37557.herokuapp.com/Wk02Home.php";
 }
