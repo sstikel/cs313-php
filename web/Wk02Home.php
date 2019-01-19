379418df985ccb10ba3da3248364625d2560e439
Samuel Gay <sstikel@gmail.com>
2019-01-18 19:18:28 -0700

    found error in home

--- web/Wk02Home.php
+++ web/Wk02Home.php
@@ -27,7 +27,7 @@
       Interests:Sam loves most anything in the outdoors; whether it be hiking, backpacking, shooting, camping, water skiing, etc.<br>
       Goals: Find a software job, get paid, have kids, take kids hiking.<br>
     </p>
-    </div>
+    
     <div class="dImages">
       <table>
         <tr><td><img id="imgSamLiz" src="SamLiz.jpg" alt="Sam and Liz">My wife and I shortly before we were married.</td></tr>
