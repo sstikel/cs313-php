3f50f6371bd4b908ac976f4888c7c4dc9747fc44
Samuel Gay <sstikel@gmail.com>
2019-01-18 18:16:14 -0700

    changed ideas

--- web/Wk02Home.php
+++ web/Wk02Home.php
@@ -2,25 +2,15 @@
 <html>
   <head>
     <title>Home-SamG-CS313</title>
-<div id="entire">    
-
-    <!--TODO External Stylesheet-->
-    <!--TODO Include colors, images, personality, creativity (keep professional)-->
     <link rel="stylesheet" type="text/css" href="Wk02SS.css"><!--TODO Alter so it works on Heroku-->
     
-    <!--TODO Add something dynamic (mouse-overs, on-click, on-change, etc)-->
-    
-    <script src="Wk02JS.js"></script><!--TODO Alter so it works on Heroku-->
+    <script src="Wk02JS.js"></script>
   </head>
 
   <header>
-    <!--Heading-->
+    <!--Heading-->    
+    <?php include 'Wk02Header.php';?>
     
-    
-      <?php include 'Wk02Header.php';?>
-    
-
-    <!--I would like a menu along the top-->
     <div class="topMenu">
     <?php include 'Wk02TopMenu.php'; ?>
     </div>
@@ -54,15 +44,7 @@
   <br>
   
   </footer>
-</div>  
 
 </html>
-
-
-<!--TODO Change file to PHP-->
-<!--TODO Add PHP content-->
-
-<!--TODO Make sure JS and CSS pages make it to server-->
-
 
 <!---->
--- web/Wk02JS.js
+++ web/Wk02JS.js
@@ -1,7 +1,5 @@
-//A simple not to know that this loaded.
-//end
 function benignFunction() {
-  getElementById("entire").innerHTML = "Maybe you can't read, but the button said 'no touch'...<br><button onclick="undo()">No Touch</button>";
+  location.replace("https://warm-refuge-37557.herokuapp.com/noTouch.html") 
 }
 
 function undo() {
