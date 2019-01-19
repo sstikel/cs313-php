7c7489242372cb3755e353167d8fb0ac117be94a
Samuel Gay <sstikel@gmail.com>
2019-01-18 17:09:08 -0700

    Top menu alterations by css and table/text for imgs

--- web/Wk02TopMenu.php
+++ web/Wk02TopMenu.php
@@ -1,5 +1,11 @@
 <?php
-  echo '<a href="Wk02Home.php">Home</a> <a href="Wk02Assign.php">Assignments</a>';
+  echo '<div class="topMenu">
+          <a href="Wk02Home.php">Home</a> 
+          <a href="Wk02Assign.php">Assignments</a>
+          <a href="#imgSamLiz"></a>
+          <a href="#imgHiking"></a>
+          <a href="#imgBallet"></a>
+        </div>';
   
-  echo "This should be the top menu";
+
 ?>
