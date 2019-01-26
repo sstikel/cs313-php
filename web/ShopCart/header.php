7843083378b8ab8575f68cad74b63492570e6e4b
Samuel Gay <sstikel@gmail.com>
2019-01-25 22:01:21 -0700

    created browse.php and header.php

---
+++ web/ShopCart/header.php
@@ -0,0 +1,9 @@
+<?php
+  //page header
+  
+  //page name
+  echo "<div id='hName'><h2>Shovel Depot</h2></div>";
+
+  //shopping cart
+  echo "Your Cart has <a href='cart.php'>$_SESSION['numCart']</a>;
+?>
