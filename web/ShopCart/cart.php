978229d66a7b8415f172b7e10c75fcb1aa566774
Samuel Gay <sstikel@gmail.com>
2019-01-26 17:36:58 -0700

    work on cart

--- web/ShopCart/cart.php
+++ web/ShopCart/cart.php
@@ -7,7 +7,7 @@
   $_SESSION[] = array("trench", 0 , 14);
   $_SESSION[] = array("snow", 0, 15);
   $_SESSION[] = array("power", 0, 50000);
-  var_dump($_SESSION);
+  
 ?>
 
 <!DOCTYPE html>
@@ -34,12 +34,14 @@
   if ($_SESSION !== null) {
     echo "Items In Cart:<br><ul>";
     foreach ($_SESSION as $a) {
-      echo "<li>" . $a[0] /*. " $" . $a[1] . ", Qty: " . $a[2] .*/ "</li><br>";
+      echo "1,";
+     // echo "<li>" . $a[0] /*. " $" . $a[1] . ", Qty: " . $a[2] .*/ "</li><br>";
     //$total += $a[2];
     //options to remove
     //option to increase qty
     }
-    echo "</ul><br><br>";
+    //echo "</ul><br><br>";
+
     ///////////total/////////////////////
     //echo "Your total: $" . $total . "<br><br>";
     
