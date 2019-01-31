2babe9c4c785009a5f2b02c7580c573967b0f41c
Samuel Gay <sstikel@gmail.com>
2019-01-31 11:07:30 -0700

    recipe db files

---
+++ DB/myDB.sql
@@ -0,0 +1,62 @@
+CREATE SCHEMA db;
+
+CREATE TABLE db.recipe (
+  id serial PRIMARY KEY,
+);
+
+
+CREATE TABLE db.author (
+  id serial PRIMARY KEY,
+  recipe_id int REFERENCES db.recipe (id),
+  name varchar(45) NOT NULL
+);
+
+CREATE TABLE db.title (
+  id serial PRIMARY KEY,
+  title varchar(45) NOT NULL,
+  recipe_id int REFERENCES db.recipe (id)
+);
+
+CREATE TABLE db.measurement (
+  id serial PRIMARY KEY,
+  measurement varchar(20) NOT NULL  
+);
+
+  --teaspoon
+  --tablespoon
+  --cup  
+  --ounce (fluid)
+  --pint
+  --quart
+  --gallon
+  --ounce (mass)
+  --pound
+
+INSERT INTO db.measurement (measurement) 
+VALUES
+      ('Teaspoon'), ('Tablespoon'), ('Cup'), 
+      ('Ounce (fluid)'), ('Pint'), ('Quart'),
+      ('Gallon'), ('Ounce (mass)'), ('Pound');
+
+CREATE TABLE db.instructions (
+  id serial PRIMARY KEY,
+  content varchar(500) NOT NULL,
+  recipe_id int REFERENCES db.recipe (id)
+);
+
+CREATE TABLE db.ingredient (
+  id serial PRIMARY KEY,
+  ingredient varchar(45) NOT NULL,
+  qty smallint NOT NULL, 
+  measurement_id int REFERENCES db.measurement (id),
+  recipe_id int REFERENCES db.recipe (id)
+);
+
+
+
+CREATE TABLE db.note (
+  id serial PRIMARY KEY,
+  recipe_id int REFERENCES db.recipe (id),
+  author_id int REFERENCES db.author (id),   --author of the note
+  content varchar(500) NOT NULL
+);
---
+++ DB/updateDB.sql
@@ -0,0 +1,60 @@
+UPDATE db.recipe (
+  id serial PRIMARY KEY,
+);
+
+
+UPDATE db.author (
+  id serial PRIMARY KEY,
+  recipe_id int REFERENCES db.recipe (id),
+  name varchar(45) NOT NULL
+);
+
+UPDATE db.title (
+  id serial PRIMARY KEY,
+  title varchar(45) NOT NULL,
+  recipe_id int REFERENCES db.recipe (id)
+);
+
+UPDATE db.measurement (
+  id serial PRIMARY KEY,
+  measurement varchar(20) NOT NULL  
+);
+
+  --teaspoon
+  --tablespoon
+  --cup  
+  --ounce (fluid)
+  --pint
+  --quart
+  --gallon
+  --ounce (mass)
+  --pound
+
+--INSERT INTO db.measurement (measurement) 
+--VALUES
+--      ('Teaspoon'), ('Tablespoon'), ('Cup'), 
+--      ('Ounce (fluid)'), ('Pint'), ('Quart'),
+--      ('Gallon'), ('Ounce (mass)'), ('Pound');
+
+UPDATE db.instructions (
+  id serial PRIMARY KEY,
+  content varchar(500) NOT NULL,
+  recipe_id int REFERENCES db.recipe (id)
+);
+
+UPDATE db.ingredient (
+  id serial PRIMARY KEY,
+  ingredient varchar(45) NOT NULL,
+  qty smallint NOT NULL, 
+  measurement_id int REFERENCES db.measurement (id),
+  recipe_id int REFERENCES db.recipe (id)
+);
+
+
+
+UPDATE db.note (
+  id serial PRIMARY KEY,
+  recipe_id int REFERENCES db.recipe (id),
+  author_id int REFERENCES db.author (id),   --author of the note
+  content varchar(500) NOT NULL
+);
