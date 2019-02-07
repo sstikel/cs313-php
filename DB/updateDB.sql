14464d618c33ba5eb74ef53e179177e4b7299283
Samuel Gay <sstikel@gmail.com>
2019-02-07 12:58:36 -0700

    team teach activity and db update

--- DB/updateDB.sql
+++ DB/updateDB.sql
@@ -1,6 +1,5 @@
-UPDATE db.recipe (
-  id serial PRIMARY KEY,
-);
+UPDATE db.recipe 
+  SET id;
 
 
 UPDATE db.author (
@@ -51,10 +50,45 @@ UPDATE db.ingredient (
 );
 
 
-
+--The note is used by people using another person's recipe and 
+-- wanting to save their own comments for the next time they use it.
 UPDATE db.note (
   id serial PRIMARY KEY,
   recipe_id int REFERENCES db.recipe (id),
   author_id int REFERENCES db.author (id),   --author of the note
   content varchar(500) NOT NULL
 );
+
+
+
+
+
+--Changes based on wk04 grading
+
+--remove title_id and instruction_id
+ALTER TABLE db.recipe 
+  DROP COLUMN title_id,
+  DROP COLUMN instructions_id;
+
+--include title, instructions, author_id
+ALTER TABLE db.recipe 
+  ADD COLUMN title varchar(45) NOT NULL,
+  ADD COLUMN instructions varchar(500) NOT NULL,
+  ADD COLUMN author_id varchar(45) NOT NULL
+;
+
+
+
+--add username & password
+--remove recipe fk
+ALTER TABLE db.author 
+  --id serial PRIMARY KEY,
+  --name varchar(45) NOT NULL,
+  ADD COLUMN username  varchar(45) NOT NULL,
+  ADD COLUMN pswrd  varchar(45) NOT NULL
+;
+
+
+--drop tables: title, instructions
+DROP TABLE IF EXISTS db.title;
+DROP TABLE IF EXISTS db.instructions;
