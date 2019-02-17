5f4dc3a08ed896da5cab5f66ee33975cae3f7b2f
Samuel Gay <sstikel@gmail.com>
2019-02-16 17:27:25 -0700

    add variables set

--- web/Recipe/recipeAdd.php
+++ web/Recipe/recipeAdd.php
@@ -11,9 +11,36 @@
   $author = htmlspecialchars($_POST['author']);
   $aPost = htmlspecialchars($_POST);
   //loop $ingredient_$count
+  $count = 0;
   foreach ($aPost as $post) {
-    //if ($post[0] != and $post[0] !=)
-    echo "<script>alert('for each worked')</script>"
+    $count += 1;
+    if (array_key_exists("ingredient_$count", $post)) {
+      $ingredient_$count = $post["ingredient_$count"];
+    }
+    //echo "<script>alert('for each worked')</script>"
   }
 
+  require_once ('../generalFiles/dbAccess.php');
+  $db = getDb();
+
+  //insert data
+  $query = 'SELECT id, title, author_id, instructions FROM db.recipe';
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $recipes = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+  //get id to redirect page
+  $query = 'SELECT id, title, author_id, instructions FROM db.recipe';
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $recipes = $statement->fetchAll(PDO::FETCH_ASSOC);
+  $id = $recipe["id"];
+
+
+  flush();
+  header("Location:recipeDetail.php?$id");//TODO - edit id...
+  die();
 ?>
