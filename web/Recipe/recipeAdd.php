42b823e8df59cd097f50f172a06e34a02be96b2a
Samuel Gay <sstikel@gmail.com>
2019-02-16 18:33:59 -0700

    debugging

--- web/Recipe/recipeAdd.php
+++ web/Recipe/recipeAdd.php
@@ -30,27 +30,27 @@
   //author
   
   //recipe
-  $query = 'INSERT INTO  db.recipe(title, author_id, instructions) VALUES()';
-  $statement = $db->prepare($query);
-  $statement->bindValue(':title', $title, PDO::PARAM_STR);
-  $statement->bindValue(':author_id', $author_id, PDO::PARAM_STR);
-  $statement->bindValue(':instructions', $instructions, PDO::PARAM_STR);
-  $statement->execute();
+  // $query = 'INSERT INTO  db.recipe(title, author_id, instructions) VALUES()';
+  // $statement = $db->prepare($query);
+  // $statement->bindValue(':title', $title, PDO::PARAM_STR);
+  // $statement->bindValue(':author_id', $author_id, PDO::PARAM_STR);
+  // $statement->bindValue(':instructions', $instructions, PDO::PARAM_STR);
+  // $statement->execute();
 
 
 
 
 
   //get id to redirect page
-  $query = 'SELECT id, title, author_id FROM db.recipe WHERE title = :title'; // . ' and author = ' . $author; //make more specific current recipe
-  $statement = $db->prepare($query);
-  $Stmt->bindParam(':title', $title, PDO::PARAM_INT);
-  $statement->execute();
-  $recipes = $statement->fetchAll(PDO::FETCH_ASSOC);
-  $id = $recipe["id"];
+  // $query = 'SELECT id, title, author_id FROM db.recipe WHERE title = :title'; // . ' and author = ' . $author; //make more specific current recipe
+  // $statement = $db->prepare($query);
+  // $Stmt->bindParam(':title', $title, PDO::PARAM_INT);
+  // $statement->execute();
+  // $recipes = $statement->fetchAll(PDO::FETCH_ASSOC);
+  // $id = $recipe["id"];
 
 
   flush();
-  header("Location:recipeDetail.php?$id");//TODO - edit id...
+ // header("Location:recipeDetail.php?$id");//TODO - edit id...
   die();
 ?>
