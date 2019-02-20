6dc42b1c4de8c13fb7785b5f08208cc6c86e52d0
Samuel Gay <sstikel@gmail.com>
2019-02-20 15:29:28 -0700

    icon

--- web/Wk02Header.php
+++ web/Wk02Header.php
@@ -1,4 +1,4 @@
 <?php
   echo "<h1>CS313 - Spring 2019<h1>";
-  echo '<link rel="icon" type="image/gif/png" href="../generalImg/sicon.png">';
+  echo '<link rel="icon" type="image/gif/png" href="/generalImg/sicon.png">';
 ?>
