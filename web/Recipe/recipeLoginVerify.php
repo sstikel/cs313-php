9a1d18309021bd9c4dabfd66315d71d7da502ab3
Samuel Gay <sstikel@gmail.com>
2019-02-22 12:51:01 -0700

    login fixes

--- web/Recipe/recipeLoginVerify.php
+++ web/Recipe/recipeLoginVerify.php
@@ -16,15 +16,20 @@ $pswrd = ($_POST['pswrd'];
 $query = $db->query("SELECT username, pswrd, name, id FROM db.author 
           WHERE username = '" . $user . "'")
 
-foreach ($query as $q) {
-  if (password_verify($pswrd, $q)) {
-    //set session variables
-    $_SESSION['name'] = $q['name'];
-    $_SESSION['user_id'] = $q['id'];
-    header('Location: /recipe/recipeHome.php')
+try {
+  foreach ($query as $q) {
+    if (password_verify($pswrd, $q)) {
+      //set session variables
+      $_SESSION['name'] = $q['name'];
+      $_SESSION['user_id'] = $q['id'];
+      header('Location: /recipe/recipeHome.php')
+    }
+    else
+      header('Location: /recipe/recipeLogin.php?fail=true');
   }
-  else
-    header('Location: /recipe/recipeLogin.php?fail=true');
 }
+catch (Exception $e) {
+  echo "error... $e";
 
+}
 ?>
