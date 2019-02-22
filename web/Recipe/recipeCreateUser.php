abca4b1d4beebf4923493b460fbf17db50a8f8ff
Samuel Gay <sstikel@gmail.com>
2019-02-22 11:58:36 -0700

    create user fixes

--- web/Recipe/recipeCreateUser.php
+++ web/Recipe/recipeCreateUser.php
@@ -16,13 +16,18 @@ $dname = $_POST['name'];
 $username = $_POST['username'];
 $pswrd = password_hash($_POST['pswrd'], PASSWORD_DEFAULT);
 
-$query = 'INSERT INTO db.author (name, username, pswrd) VALUES (:dname, :username, :pswrd)';
-$statement = $db->prepare($query);
-$statement->bindValue(':dname', $dname);
-$statement->bindValue(':username', $username);
-$statement->bindValue(':pswrd', $pswrd);
-$statement->execute();
-
+try {
+  $query = 'INSERT INTO db.author (name, username, pswrd) VALUES (:dname, :username, :pswrd)';
+  $statement = $db->prepare($query);
+  $statement->bindValue(':dname', $dname);
+  $statement->bindValue(':username', $username);
+  $statement->bindValue(':pswrd', $pswrd);
+  $statement->execute();
+}
+catch (Exception $e) {
+  echo "db error... $e";
+  die();
+}
 
 header('Location: recipeHome.php');
 die();
