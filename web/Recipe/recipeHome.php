27014f4e85291a3619587be6bf557e4d02fa6653
Samuel Gay <sstikel@gmail.com>
2019-02-22 16:03:12 -0700

    recipe author id

--- web/Recipe/recipeHome.php
+++ web/Recipe/recipeHome.php
@@ -35,7 +35,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/gif/png" href="../generalImg/sicon.png"> <!--add to all other pages-->
+    <link rel="icon" type="image/gif/png" href="../generalImg/sicon.png">
     <link rel="stylesheet" type="text/css" href="../generalFiles/generalSS.css">
     <script type="text/javascript" src="../generalFiles/generalJS.js"></script>
     <title>Recipe Home</title>
@@ -83,7 +83,7 @@
   <div class="userSignedIn" id="dfAdd">
   <form action="recipeAdd.php" method="post">
     Title: <input type="text" name="title">
-    <input type="hidden" name ="author" value="<?php $user_id ?>"><br> <!--use php to insert user info-->
+    <?php echo "<input type='hidden' name ='author' value='$user_id'><br>"; ?>
     <table>
       <tr><th>Ingredients:</th><th>Quantity</th><th>Measurment</th></tr>
       <?php
