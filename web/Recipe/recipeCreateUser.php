6ac9ad3c707431e4ef6703ec0f7e307b5f0849f7
Samuel Gay <sstikel@gmail.com>
2019-02-22 11:46:11 -0700

    create user fixes

--- web/Recipe/recipeCreateUser.php
+++ web/Recipe/recipeCreateUser.php
@@ -12,8 +12,8 @@ session_start();
 require_once ('../generalFiles/dbAccess.php');
 $db = getDb();
 
-$dname = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
-$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+$dname = $_POST['name'];
+$username = $_POST['username'];
 $pswrd = password_hash($_POST['pswrd'], PASSWORD_DEFAULT);
 
 $query = 'INSERT INTO db.author (name, username, pswrd) VALUES (:dname, :username, :pswrd)';
